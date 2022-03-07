e71ec8f4633be747f4522754a8c859cd54a1e92e
Marvin <trujillomarvin@hotmail.com>
2022-03-07 15:19:23 -0800

    getting somewhere finally. slept on it ya know?

--- app.js
+++ app.js
@@ -1,33 +1,86 @@
 const log = console.log;
+
 const quadrant = document.querySelectorAll(".quadrant");
 let playerChoice = document.querySelectorAll(".playerChoice");
 
 
-
 const gameBoard = (() => {
-
-    const playerMark = () => {
-        return "this"
-    }
-
-    playerChoice.forEach(button => {
-        button.addEventListener("click", playerMark);
-    });
 
     const playerFactory = (name, mark, turn) => {
         return { name, mark, turn };
     }
 
-    const player1 = playerFactory('player1', 'X', true);
-    const player2 = playerFactory('player2', 'O', false);
+    let player1 = playerFactory('player1', undefined, true);
+    let player2 = playerFactory('player2', undefined, false);
 
 
 
 
-    return { playerMark }
+    // MARKING THE GAMEBOARD //
+    function placeMark() {
+        if (player1.mark == "X" && this.lastChild == null) {
+            let newX = document.createElement("img");
+            newX.setAttribute("src", "/icons/x.svg")
+            this.appendChild(newX);
+        }
+    }
+
+    quadrant.forEach(cell => {
+        cell.addEventListener("click", placeMark)
+    });
+
+
+    // PLAYER SELECTION //
+    const assignPlayer = function () {
+        player1.mark = this.value;
+        if (player1.mark == "X") {
+            player2.mark = "O"
+        }
+        else player2.mark = "X"
+        log(gameBoard);
+    }
+
+    playerChoice.forEach(button => {
+        button.addEventListener("click", assignPlayer);
+    });
+
+
+    let winningCombos = [
+        //ROWS//
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        //COLUMNS//
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        //DIAGONALS//
+        [0, 4, 8],
+        [6, 4, 2]
+    ]
+
+    return { player1, player2, winningCombos }
 })();
 
 
+
+
+
+// let winningCombos = [
+//     //ROWS//
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8],
+//     //COLUMNS//
+//     [0, 3, 6],
+//     [1, 4, 7],
+//     [2, 5, 8],
+//     //DIAGONALS//
+//     [0, 4, 8],
+//     [6, 4, 2]
+// ]
+
+// log(winningCombos);
 
 
 
@@ -41,3 +94,29 @@ const gameBoard = (() => {
 
 
 
+// ======================= TEST GROUNDS ================== //
+
+// function EmployeeDetails() {
+//     var name = "Mayank";
+//     var age = 30;
+//     var designation = "Developer";
+//     var salary = 10000;
+
+//     var calculateBonus = function (amount) {
+//         return salary = salary + amount;
+//     }
+
+//     return {
+//         name: name,
+//         age: age,
+//         designation: designation,
+//         calculateBonus: calculateBonus,
+//         salary
+//     }
+// }
+
+// var newEmployee = EmployeeDetails()
+
+// var userName = newEmployee.calculateBonus(1000);
+
+//========================================================//
